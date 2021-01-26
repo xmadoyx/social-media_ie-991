07f4f852f9ca1972e71b87e483771af7c47f2a0f
madoy <imiNfiNit3@yahoo.com>
2021-01-26 11:52:40 +0330

    main storage for specified components

---
+++ src/context/TweetContext.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+var TweetStateContext = React.createContext();
+var TweetDispatchContext = React.createContext();
+
+function tweetReducer(state, action) {
+  switch (action.type) {
+    case "SET_TWEET_TEXT":
+      return  {...state, tweetText: action.payload};
+    case "SET_TWEET_LIST":
+        return {...state, tweetList: action.payload}; 
+    case "LIKE_TWEET":
+      const tweetId = action.payload;
+      const foundIndex = state.tweetList.findIndex(item => item._id === tweetId);
+      if (foundIndex === -1) {
+          return {state};
+      }else{
+      return {...state, tweetList : [...state.tweetList.slice(0,foundIndex),{...state.tweetList[foundIndex], likes : state.tweetList[foundIndex].likes + 1 }, ...state.tweetList.slice(foundIndex+1)]}
+    }
+    case "TOGGEL_DRAWER":
+      return {...state, toggelDrawer : !state.toggelDrawer}
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+function TweetProvider({children}) {
+  var [state, dispatch] = React.useReducer(tweetReducer, {
+    tweetText :  '',
+    tweetList : [] ,
+    toggelDrawer : true,
+  });
+  return (
+    <TweetStateContext.Provider value={state}>
+      <TweetDispatchContext.Provider value={dispatch}>
+        {children}
+      </TweetDispatchContext.Provider>
+    </TweetStateContext.Provider>
+  );
+}
+
+function useTweetState() {
+  var context = React.useContext(TweetStateContext);
+  if (context === undefined) {
+    throw new Error("useTweettState must be used within a TweetProvider");
+  }
+  return context;
+}
+
+function useTweetDispatch() {
+  var context = React.useContext(TweetDispatchContext);
+  if (context === undefined) {
+    throw new Error("useTweetDispatch must be used within a TweetProvider");
+  }
+  return context;
+}
+
+export {TweetProvider, useTweetState, useTweetDispatch, setText, likeTweet, setTweetList, setToggelDrawer};
+
+//here is our funtions
+
+function setText(dispatch,tweetText) {
+    dispatch({
+        type : "SET_TWEET_TEXT",
+        payload : tweetText
+    });
+}
+
+function likeTweet(dispatch,id) {
+    dispatch({
+        type : "LIKE_TWEET",
+        payload : id
+    });
+}
+
+function setTweetList(dispatch,list) {
+    dispatch({
+        type : "SET_TWEET_LIST",
+        payload : list
+    });
+}
+
+function setToggelDrawer(dispatch) {
+  dispatch({
+      type : "TOGGEL_DRAWER",
+  });
+}
+
+
