60af129e479ecce146f12288dcd97b946bd9880d
madoy <imiNfiNit3@yahoo.com>
2021-01-26 07:33:11 +0330

    header components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import useStyles from './style';
+
+const Header = ({icon,title}) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.header}>
+            {icon}
+            <Typography className={classes.headerTitle}>{title}</Typography>
+        </div>
+    );
+};
+
+export default Header;
