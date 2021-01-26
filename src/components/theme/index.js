26c6b4dfbe30d8215d3de4a9d9e89c43b833e9fd
madoy <imiNfiNit3@yahoo.com>
2021-01-26 07:49:29 +0330

    general theme and design

---
+++ src/components/theme/index.js
@@ -0,0 +1,10 @@
+import {createMuiTheme} from "@material-ui/core";
+const Theme = createMuiTheme({
+    root: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+      ].join(','),    
+    },
+  });
+  export default Theme;
