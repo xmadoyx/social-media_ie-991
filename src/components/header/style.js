111943e2b81a17990686bba0cd16cdabe4d0edb7
madoy <imiNfiNit3@yahoo.com>
2021-01-26 07:33:40 +0330

    styling header components

---
+++ src/components/header/style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    
+    header :{
+        display : 'flex',
+        padding : 18,
+        backgroundColor : 'white'
+    },
+    headerTitle : {
+        marginLeft : 10,
+        fontWeight : 600
+    }, 
+});
+export default useStyles;
